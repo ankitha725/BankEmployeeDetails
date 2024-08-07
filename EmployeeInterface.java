1e26bb24e14fda7db946789c9fe88f5a63a27a13
ankitha725 <155876705+ankitha725@users.noreply.github.com>
2024-08-07 19:06:21 +0530

    EmployeeInterface.java
    interface

---
+++ EmployeeInterface.java
@@ -0,0 +1,7 @@
+pacakge Myapplication;
+public interface EmployeeInterface{
+  public void empinfo(int id,String name,double sal);
+  public void empadd(String add);
+  public void empdetails();
+}
+}
